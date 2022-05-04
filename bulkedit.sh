a720d71b4294cf8abbc7a87374692b8b09b5bd0e
Penrose Library. Whitman College <102998763+PenroseLib@users.noreply.github.com>
2022-05-04 13:49:55 -0700

    Create bulkedit.sh

---
+++ bulkedit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ------------------------------------------
+while IFS=',' read -r permanent_url pid; do
+        echo "MODIFY 10349/$pid"
+        echo "100 HS_ADMIN 86400 1110 ADMIN 200:011111110011:0.NA/10349"
+		    echo "3 URL 86400 1110 UTF8 $permanent_url"
+		    printf "\n"
+done < "$1"
